738f00e4941ce40d92437c52e29b47d6e273cf91
Alejandro Vallejos Vitalevi <alejandrovallejos123@gmail.com>
2026-08-29 15:21:11 -0300

    Agregar findByActivoTrue al ProductoRepository

--- src/main/java/com/uade/e_commerce/repository/ProductoRepository.java
+++ src/main/java/com/uade/e_commerce/repository/ProductoRepository.java
@@ -1,13 +1,20 @@
 package com.uade.e_commerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uade.e_commerce.model.Producto;
+
 /**
  * JPA repository interface for managing Producto entities
  * Provides CRUD operations and query methods for interacting with the database
  * ProductoRepository
  * save, update, delete, findById, findAll, etc.
  */
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    List<Producto> findByActivoTrue();
 }

af442e6cbd7d6da80824e912913cc49648936bf6
Alejandro Vallejos Vitalevi <alejandrovallejos123@gmail.com>
2026-08-29 15:21:11 -0300

    Agregar stock, imagenUrl y activo a la entidad Producto

--- src/main/java/com/uade/e_commerce/model/Producto.java
+++ src/main/java/com/uade/e_commerce/model/Producto.java
@@ -1,15 +1,18 @@
 package com.uade.e_commerce.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -19,10 +22,21 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(nullable = false)
     private String nombre;
+
     private String descripcion;
+
+    @Column(nullable = false)
     private Double precio;
 
-    
+    @Column(nullable = false)
+    private Integer stock;
+
+    private String imagenUrl;
+
+    @Column(nullable = false)
+    private Boolean activo = true;
+
 }

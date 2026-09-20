769f7e22f49a1e5cd46259198322786ab34adb8b
nicolasalgranti <nicolasalgranti@gmail.com>
2026-09-19 21:45:16 -0300

    Agregar AuthResponse, ActualizarUsuarioRequest y validaciones (@Email, @Size) en Registro/LoginRequest

---
+++ src/main/java/com/uade/e_commerce/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.uade.e_commerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String tipo;
+    private long expiraEn;
+}
